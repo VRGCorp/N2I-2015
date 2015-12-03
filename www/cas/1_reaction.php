a39726cdffcdac7f11a6a37bd808ef422023c5b6
Bilal EL YASSEM <shwarthog4@gmail.com>
2015-12-03 23:55:01 +0100

    Modification des fichiers du template de l'Incendie (1).

---
+++ www/cas/1_reaction.php
@@ -0,0 +1,4 @@
+En cas d'incendie il est très important de prévenir tout le monde et d'appeler les pompiers (téléphoner au 18 ou au 112).
+Si l'on se trouve dans un bâtiment et qu'un incendie se déclare il faut garder son calme et se diriger vers la sortie la plus proche.
+Se baisser est aussi essentiel car les fumées toxiques montent vers le plafond, l'air est donc plus respirable près du sol.
+Une fois à l'extérieur se réunir en sécurité et attendre les secours.
