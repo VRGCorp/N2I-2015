14238a687b84366037b05dfc1a9c1b1bf4ae07ac
kloumpt <ubunthero@gmail.com>
2015-12-03 11:27:41 +0100

    Configuration du dépot

---
+++ www/index.php
@@ -0,0 +1,11 @@
+<?php
+	error_reporting(E_ALL);
+	ini_set("display_errors", 1);
+?>
+<?php
+
+echo "cloning";
+exec("cd ..; rm ./git -rf; git clone https://github.com/kloumpt/N2I.git ./git; cp -r ./git/www/* www/");
+echo "done";
+ 
+
