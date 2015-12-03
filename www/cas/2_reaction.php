3bb51b86f6ba709e44f0b13b0fca32cb006f5346
Timothée Ficat <timothee.ficat@laposte.net>
2015-12-04 00:01:41 +0100

    Explication des séismes, réactions, idée de jeu et webographie dans la doc.

---
+++ www/cas/2_reaction.php
@@ -0,0 +1,6 @@
+Pour se protéger d'un tremblement de terre, la meilleure réaction est de se réfugier sous une table pour éviter d'être blessé par des chutes de débris.
+En effet, le plus gros risque est qu'un bâtiment s'effondre ou qu'un objet lourd tombe.
+
+Au Japon il y a environ 300 séismes par an. Les bâtiments sont donc construit sur les bases en caoutchouc pour résister aux vibrations et ne pas s'effondrer.
+
+Enfin, les tremblements de terre sont rarements très destructeurs, il ne faut donc rester calme et sortir des bâtiments sans précipitation.
