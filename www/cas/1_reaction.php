359a8aad1c655b3185688129aee1498299b503b6
Timothée Ficat <timothee.ficat@laposte.net>
2015-12-04 06:14:14 +0100

    Update cas 1

--- www/cas/1_reaction.php
+++ www/cas/1_reaction.php
@@ -2,5 +2,5 @@
 Si l'on se trouve dans un bâtiment et qu'un incendie se déclare il faut garder son calme et se diriger vers la sortie la plus proche.<br/>
 Se baisser est aussi essentiel car les fumées toxiques montent vers le plafond, l'air est donc plus respirable près du sol.<br/>
 Une fois à l'extérieur il faut se réunir en sécurité et attendre les secours.<br/>
-<img src="images/logo_pt_rassemblement.jpg"/>
+<img src="images/logo_pt_rassemblement.jpg" class="illustration"/>
 </p>
