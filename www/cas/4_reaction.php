0676734ab5ffd31c96a9f8c19eb695b329f10692
Timothée Ficat <timothee.ficat@laposte.net>
2015-12-04 03:20:14 +0100

    Mise en page du texte des cas 2 et 4.

--- www/cas/4_reaction.php
+++ www/cas/4_reaction.php
@@ -1,5 +1,11 @@
+<p>
 Si le niveau de l'eau baisse rapidement il y a de fortes chances qu'un tsunami approche de la côte.
+</p>
 
+<p>
 Pour s'en protéger, il faut s'éloigner des zones proches de l'eau et prendre de la hauteur en grimpant dans un arbre ou sur une coline par exemple.
+</p>
 
+<p>
 Emporté par le courant, il est important de s'accrocher à un objet flottant pour rester à la surface.
+</p>
