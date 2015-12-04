b724bb5c510e69d283aab091779812c94da04652
Bilal EL YASSEM <shwarthog4@gmail.com>
2015-12-04 04:57:57 +0100

    Corrections orthographiques.

--- www/cas/2_reaction.php
+++ www/cas/2_reaction.php
@@ -4,9 +4,9 @@ En effet, le plus gros risque est qu'un bâtiment s'effondre ou qu'un objet lour
 </p>
 
 <p>
-Au Japon il y a environ 300 séismes par an. Les bâtiments sont donc construit sur des bases en caoutchouc pour résister aux vibrations et ne pas s'effondrer.
+Au Japon il y a environ 300 séismes par an. Les bâtiments sont donc construits sur des bases en caoutchouc pour résister aux vibrations et ne pas s'effondrer.
 </p>
 
 <p>
-Enfin, les tremblements de terre sont rarements très destructeurs, il ne faut donc rester calme et sortir des bâtiments sans précipitation.
+Enfin, les tremblements de terre sont rarements très destructeurs, il faut donc rester calme et sortir des bâtiments sans précipitation.
 </p>
--- www/cas/3_reaction.php
+++ www/cas/3_reaction.php
@@ -1,4 +1,4 @@
-<p>Le danger est l'air contaminé, si il est dehors il faut se réfugier d'urgence dans un bâtiment.<br/>
+<p>Le danger est l'air contaminé, s'il est dehors il faut se réfugier d'urgence dans un bâtiment.<br/>
 Une fois à l'intérieur il est très important de fermer les fenêtres et les portes et d'empêcher l'air d'entrer dans l'abri.<br/>
 Si l'air toxique est à l'intérieur il faut vite sortir du bâtiment et respirer le moins possible.<br/>
 Une fois en sécurité appeler les secours (le 18 ou le 112).</p>
