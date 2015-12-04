d9be7dc0aaa2cee2cdf49ebea0bf9bffcd8c9da4
kloumpt <serge.molina+github@kloumpt.net>
2015-12-04 02:14:58 +0100

    BugFix of pointer position on the games

--- www/cas/1_jeu.php
+++ www/cas/1_jeu.php
@@ -3,5 +3,6 @@
 		var case_id="1";
 	</script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="http://hammerjs.github.io/dist/hammer.min.js"></script>
 	<script src="/cas/X_game.js"></script>
 </div>
--- www/cas/2_jeu.php
+++ www/cas/2_jeu.php
@@ -3,5 +3,6 @@
 		var case_id="2";
 	</script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="http://hammerjs.github.io/dist/hammer.min.js"></script>
 	<script src="/cas/X_game.js"></script>
 </div>
