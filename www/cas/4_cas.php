971eff00073fac8f15642a80666b07c72ef05865
Timothée Ficat <timothee.ficat@laposte.net>
2015-12-04 01:33:33 +0100

    Fin du cas 4.

--- www/cas/4_cas.php
+++ www/cas/4_cas.php
@@ -1,2 +1,4 @@
-Un tsunami est une catastrophe naturelle pendant laquelle une grande vague recouvre brusquement les terres.
-La vitesse et la taille de la vague très imposante provoque beaucoup de dégats en détruisant des batîments et des véhicules.
+Un tsunami est une catastrophe naturelle pendant laquelle une grande vague recouvre brusquement la côte.
+La vitesse et le poids de la grande vague provoque beaucoup de dégâts et peut même détruire des bâtiments et emporter des véhicules.
+
+Les tsunamis sont causés par des éruptions ou des séismes sous-marins de grande importance.
---
+++ www/cas/4_reaction.php
@@ -0,0 +1,5 @@
+Si le niveau de l'eau baisse rapidement il y a de fortes chances qu'un tsunami approche de la côte.
+
+Pour s'en protéger, il faut s'éloigner des zones proches de l'eau et prendre de la hauteur en grimpant dans un arbre ou sur une coline par exemple.
+
+Emporté par le courant, il est important de s'accrocher à un objet flottant pour rester à la surface.
