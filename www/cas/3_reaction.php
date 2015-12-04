7418fd90e8ef3f697c82cd65503bbb865452fa5c
Bilal EL YASSEM <shwarthog4@gmail.com>
2015-12-04 01:10:16 +0100

    Ajout template cas risque chimique.

--- www/cas/3_reaction.php
+++ www/cas/3_reaction.php
@@ -1,2 +1,4 @@
 Le danger est l'air contaminé, si il est dehors il faut se réfugier d'urgence dans un bâtiment.
-Si il est à l'intérieur il faut vite sortir du bâtiment.
+Une fois à l'intérieur il est très important de fermer les fenêtres et les portes et d'empêcher l'air d'entrer dans l'abri.
+Si l'air toxique est à l'intérieur il faut vite sortir du bâtiment et respirer le moins possible.
+Une fois en sécurité appeler les secours (le 18 ou le 112).
