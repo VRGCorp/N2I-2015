610ea5f03ba4276635125b24865798d47966c103
Timothée Ficat <timothee.ficat@laposte.net>
2015-12-04 06:10:33 +0100

    Update cas 1

--- www/cas/1_reaction.php
+++ www/cas/1_reaction.php
@@ -1,4 +1,6 @@
 <p>En cas d'incendie il est très important de prévenir tout le monde et d'appeler les pompiers (téléphoner au 18 ou au 112).<br/>
 Si l'on se trouve dans un bâtiment et qu'un incendie se déclare il faut garder son calme et se diriger vers la sortie la plus proche.<br/>
 Se baisser est aussi essentiel car les fumées toxiques montent vers le plafond, l'air est donc plus respirable près du sol.<br/>
-Une fois à l'extérieur il faut se réunir en sécurité et attendre les secours.</p>
+Une fois à l'extérieur il faut se réunir en sécurité et attendre les secours.
+<img src="images/logo_pt_rassemblement.jpg"/>
+</p>
